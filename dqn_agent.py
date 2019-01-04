1a428bdb571031dbac96790d2bfa505129ee116f
rohbot <rohanoahmad@gmail.com>
2019-01-04 14:58:37 +0000

    added Navigation and model weights

--- dqn_agent.py
+++ dqn_agent.py
@@ -8,12 +8,12 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 128         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate 
-UPDATE_EVERY = 4        # how often to update the network
+LR = 0.0001               # learning rate 
+UPDATE_EVERY = 2        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
